1fec9a5f235f02edd6cfa93106c3f42bb443824d
chieby-mwangi <chiebyrah@gmail.com>
2024-10-05 07:12:29 +0300

    phase-0-intro-to-js-2-looping-code-along git

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+// index.js
+
+// Function to write cards
+function writeCards(namesArray, event) {
+  let messages = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    messages.push(
+      `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`
+    );
+  }
+  return messages;
+}
+
+// Function to count down
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+// index.js
+
+module.exports = {
+  writeCards,
+  countDown,
+};
